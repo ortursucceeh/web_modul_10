2ca7a4a36d7210be31350b041ad4d15c820e7044
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-02-03 15:27:45 +0200

    describe models

--- quote_site/models.py
+++ quote_site/models.py
@@ -3,10 +3,27 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
-    pass
+    fullname = models.CharField(max_length=200)
+    borndate = models.DateField()
+    born_location = models.CharField(max_length=200)
+    description = models.TextField()
 
-class Quote(models.Model):
-    pass
+    def __str__(self):
+        return self.fullname
 
 class Tag(models.Model):
-    pass
+    caption = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.caption
+
+class Quote(models.Model):
+    text = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="quotes")
+    
+    def __str__(self):
+        return f"{self.author.fullname} {self.text[:20]}"
+
