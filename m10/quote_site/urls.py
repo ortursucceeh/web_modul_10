1007f0f86c25c89dec4d2c2a10811415036b4a54
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-02-03 19:12:59 +0200

    add author-detail page and view

--- m10/quote_site/urls.py
+++ m10/quote_site/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'quote_site'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('<int:id>/', views.author_detail, name='author_detail'),
 ]
--- m10/quote_site/views.py
+++ m10/quote_site/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from .models import Quote, Tag
+from .models import Quote, Tag, Author
 # Create your views here.
 def index(request):
-
     quotes = Quote.objects.all()
     
     return render(request, "quote_site/index.html", {"quotes": quotes})
+
+def author_detail(request, id):
+    author = Author.objects.get(pk=id)
+
+    return render(request, "quote_site/author_detail.html", {"author": author})
+
+
+def add_quote(request):
+    pass
+
+def add_author(request):
+    pass
